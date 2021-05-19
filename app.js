4ca1a889a0f787fb0b22072e77f01485ac5c52f0
Fred <floony7@yahoo.com>
2021-05-19 21:53:20 +1200

    Created views folder and set up Handlebars templates

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require("express")
-
+const exphbs = require("express-handlebars")
+const mongoose = require("mongoose")
 const app = express()
 
+// Handlebars Middleware
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+  })
+)
+app.set("view engine", "handlebars")
+
 app.get("/", (req, res) => {
-  res.send("Index page")
+  const title = "Vitejte"
+  res.render("index", {
+    title,
+  })
+})
+
+app.get("/about", (req, res) => {
+  res.render("about")
 })
 
 const PORT = process.env.PORT || 5000
